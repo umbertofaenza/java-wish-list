8aee3549110cb8b9a1de23afc8820a930ff436e8
umbertofaenza <umberto.faenza@gmail.com>
2024-04-05 15:32:08 +0200

    feat: wishlist file reading and writing

--- src/org/learning/javawishlist/Gift.java
+++ src/org/learning/javawishlist/Gift.java
@@ -3,7 +3,8 @@ package org.learning.javawishlist;
 public class Gift implements Comparable<Gift>{
     private String name;
 
-    public Gift(String name) {
+    public Gift(String name) throws IllegalArgumentException {
+        validateString(name);
         this.name = name;
     }
 
@@ -21,5 +22,11 @@ public class Gift implements Comparable<Gift>{
     @Override
     public int compareTo(Gift g) {
         return this.getName().compareTo(g.getName());
+    }
+
+    private void validateString(String s) {
+        if (s == null || s.isEmpty()) {
+            throw new IllegalArgumentException("Invalid input");
+        }
     }
 }
--- src/org/learning/javawishlist/Main.java
+++ src/org/learning/javawishlist/Main.java
@@ -1,15 +1,32 @@
 package org.learning.javawishlist;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        File wishlistFile = new File("./resources/wishlist.txt");
+        List<Gift> wishList = new ArrayList<>();
+
         Scanner scan = new Scanner(System.in);
 
-        List<Gift> wishList = new ArrayList<>();
+        try(Scanner fileReader = new Scanner(wishlistFile)) {
+            while(fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                Gift gift = new Gift(line);
+
+                wishList.add(gift);
+            }
+        } catch(FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
 
         boolean exit = false;
         while(!exit) {
@@ -18,10 +35,15 @@ public class Main {
 
             if (choice.equalsIgnoreCase("y")){
                 System.out.print("Gift name: ");
-                String giftName = scan.nextLine();
 
-                Gift gift = new Gift(giftName);
-                wishList.add(gift);
+                try{
+                String giftName = scan.nextLine();
+                    Gift gift = new Gift(giftName);
+                    wishList.add(gift);
+                } catch(IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                }
+
             } else if (choice.equalsIgnoreCase("n")) {
                 exit = true;
             } else {
@@ -30,7 +52,16 @@ public class Main {
         }
 
         if(!wishList.isEmpty()) {
-            Collections.sort(wishList);
+            //Collections.sort(wishList);
+
+            try(FileWriter fileWriter = new FileWriter(wishlistFile)) {
+                for(Gift gift : wishList) {
+                    fileWriter.write(gift.getName() + "\n");
+                }
+            } catch(IOException e) {
+                System.out.println(e.getMessage());
+            }
+
             System.out.println(wishList);
         }
 
